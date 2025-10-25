6e8017e459c49db24b334b302aa8d7a3c7f8e778
Raiane <raiane.rze@gmail.com>
2025-10-24 23:43:17 -0300

    deletes one br

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,7 +4,6 @@ const Header: React.FC = () => {
   return (
     <header className="text-center py-6 md:py-8">
       <br />
-      <br />
       <a href="/">
       <h1 className="text-4xl md:text-5xl font-extrabold tracking-wider text-blue-900" style={{ textShadow: '0 0 10px #cdd3fbff, 0 0 20px #fceebbff' }}>
         Celestial Insights
